0b4aeb7ca7d1ac8b3eddca1ad82eb7f7681273c0
SavinduK <93889270+SavinduK@users.noreply.github.com>
2024-02-15 09:55:45 +0530

    Add files via upload

---
+++ full stack web developer 1/script.js
@@ -0,0 +1,36 @@
+function setTime(){
+    const dateTag = document.getElementById("date")
+    let date = new Date().toLocaleString()
+    date = date.replace(",","-")
+    dateTag.innerHTML = date
+}
+setInterval(setTime,1000)
+
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
